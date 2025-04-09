67b53760ad92b45b0501113ecc3fafc03a191455
yamini200403 <yaminiarumugam2004@gmail.com>
2025-04-09 10:35:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyD64fXyTqLjHQQAeOGrZTkS76qVGXwWY8Q",
     authDomain: "instagram-67f16.firebaseapp.com",
     projectId: "instagram-67f16",
-    #storageBucket: "instagram-67f16.firebasestorage.app",
+    #storageBucket: "instagram-67f16.firebasestorage.app"#,
     messagingSenderId: "412774640094",
     appId: "1:412774640094:web:07a477453f59125d72954f"
   };
